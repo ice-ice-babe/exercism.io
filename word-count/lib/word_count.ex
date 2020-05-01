b5df8d82d801209f1e1975f0d3c3781435559cdd
ice-ice-babe <stojanovicm1999@gmail.com>
2020-05-01 02:21:34 +0200

    .

---
+++ word-count/lib/word_count.ex
@@ -0,0 +1,18 @@
+defmodule WordCount do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    sentence
+    |> String.downcase
+    |> String.replace(~r/[ !"$%&,:@^_]+/," ")
+    |> String.split
+    |> Enum.reduce(%{},
+    fn(word, acc) ->
+      Map.put(acc, word, Map.get(acc, word, 0) +1)
+    end)
+  end
+end
